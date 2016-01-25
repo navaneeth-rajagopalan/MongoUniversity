6c818c2aaf273269b75e524c48a94e676938194a
Navaneeth R <navaneethnvnthr@gmail.com>
2016-01-25 10:25:50 +0530

    week 3 lecture 3

--- Week 3/HandsOn/app.js
+++ Week 3/HandsOn/app.js
@@ -3,16 +3,19 @@ app = express(),
 mongodb = require('mongodb'),
 mongoClient = mongodb.MongoClient,
 assert = require('assert'),
-engines = require('consolidate');
+engines = require('consolidate'),
+commandLineArgs = require('command-line-args');
 
 app.engine('html',engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views',__dirname+'/views');
 
+var options = commandLineOptions(),
+query = generateQuery(options);
 
 mongoClient.connect("mongodb://localhost:27017/crunchbase", function(err, db){
     assert.equal(err, null);
-    var query = {"category_code":"web"};
+    var projection = {"_id":0, "founded_year":1, "name":1, "number_of_employees":1};
     // Using arrays
     // db.collection("companies").find(query).toArray(function(err, companies){
     //     assert.equal(err, null);
@@ -25,13 +28,43 @@ mongoClient.connect("mongodb://localhost:27017/crunchbase", function(err, db){
     // });    
 
     // Using cursor.forEach
-    var cursor = db.collection("companies").find(query);
+    var cursor = db.collection("companies").find(query).project(projection);
     cursor.forEach(function(company) {
-        console.log(company.name+" is a "+company.category_code+" company.");
-    }, 
-    function(err){
+        //console.log(company.name+" is a "+company.category_code+" company.");
+        console.log(company);
+    },     
+    function(err) {
         assert.equal(err, null);
+        console.log("Our query was:" + JSON.stringify(query));
         return db.close();
-    });       
-
+    }
+    );
 });
+
+function commandLineOptions(){
+    var cli = commandLineArgs([
+        {name: "firstYear", alias: "f", type:Number},
+        {name: "lastYear", alias: "l", type:Number},
+        {name: "employees", alias: "e", type:Number}
+    ]);    
+    var options = cli.parse();  
+    if(!("firstYear" in options && "lastYear" in options)){
+        console.log(cli.getUsage({
+           title:"Usage",
+           description: "The first two options below are required. The rest are optional." 
+        }));
+        process.exit();
+    }  
+    return options;   
+}
+
+function generateQuery(options){
+    var query = {
+        "founded_year": {"$gte":options.firstYear, "$lte":options.lastYear}
+    };
+    if ("employees" in options){
+        query.number_of_employees = {"$lte":options.employees};
+    }
+    console.log(query);
+    return query;
+}
